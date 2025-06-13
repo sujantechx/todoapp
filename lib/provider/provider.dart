bd8eb7b036dfacee62f5e708fb11ea15dedea954
Sujan <sujantechx@gmail.com>
2025-06-13 20:38:43 +0530

    Initial commit

---
+++ lib/provider/provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DbProvider extends ChangeNotifier{
+  DbProvider dbHelper;
+  DbProvider({required this.dbHelper});
+
+  List<Map<String ,dynamic>> _allTodo=[];
+  List<Map<String,dynamic>> getData=[];
+
+
+  /// event
+  addTodo({required String title, required String desc, required int priority, int filter = 0}) async{
+
+    bool check= await dbHelper.addTodo(title: title, desc: desc, priority: priority);
+    if(check){
+      _allTodo=await dbHelper.featchAllTodo(filter);
+      notifyListeners()
+    }
+  }}
