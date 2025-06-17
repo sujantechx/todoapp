cbacf34d644248f1639f01e184f195c2dcfed491
Sujan <sujantechx@gmail.com>
2025-06-17 20:49:12 +0530

    using provider GMS

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoapp/datbase/database_helper.dart';
 import 'package:todoapp/loginPage/login_page.dart';
-
+import 'package:todoapp/provider/provider.dart';
 void main() {
-  runApp( MyApp());
+  runApp(
+    ChangeNotifierProvider<DbProvider>(
+      create: (context) => DbProvider(
+        dbHelper: DBHelper.getInstance(),
+      ),
+      child: MyApp(),
+    ),
+  );
 }
-class MyApp extends StatelessWidget{
+/*
+void main() {
+  runApp(
+    /// multiple provider r
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<DbProvider>(
+          create: (context) => DbProvider(
+            dbHelper: DBHelper.getInstance(),
+          ),
+          child: MyApp(),
+        ),
+      ],
+    ));
+      /*
+        ChangeNotifierProvider<DbProvider>(
+          create: (context) => DbProvider(
+            dbHelper: DBHelper.getInstance(),
+          ),
+          child: MyApp(),
+        ),
+     );*/
+}
+*/
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:"ToDo Now" ,
-      // color:Colors.blue ,
+      title: "ToDo Now",
       home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
