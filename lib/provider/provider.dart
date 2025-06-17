2ed3d095d5d0d54ecedb6a778ce19642c90a178f
Sujan <sujantechx@gmail.com>
2025-06-17 20:49:03 +0530

    using provider GMS

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,21 +1,45 @@
-// import 'package:flutter/material.dart';
-//
-// class DbProvider extends ChangeNotifier{
-//   DbProvider dbHelper;
-//   DbProvider({required this.dbHelper});
-//
-//   List<Map<String ,dynamic>> _allTodo=[];
-//   List<Map<String,dynamic>> getData=[];
-//
-//
-//   /// event
-//   addTodo({required String title, required String desc, required int priority, int filter = 0}) async{
-//
-//     bool check= await dbHelper.addTodo(title: title, desc: desc, priority: priority);
-//     if(check){
-//       _allTodo=await dbHelper.featchAllTodo(filter );
-//       notifyListeners();
-//     }
-//   }
-//
-//   featchAllTodo(int filter, {required int filter}) {}}
+import 'package:flutter/material.dart';
+import 'package:todoapp/datbase/database_helper.dart';
+
+class DbProvider extends ChangeNotifier{
+  DbProvider({required this.dbHelper});
+  // DbProvider dbHelper; this prblem
+  final DBHelper dbHelper;
+  List<Map<String ,dynamic>> _allTodo=[];
+  List<Map<String,dynamic>> getData()=>_allTodo;
+
+  // event
+
+  addTodo({
+    required String title,
+    required String desc,
+    required int priority,
+    required String deadline,
+    int filter = 0,
+  }) async{
+    bool check = await dbHelper.addTodo(
+      title: title,
+      desc: desc,
+      deadline: deadline,
+      priority: priority,
+    );
+    if(check){
+      _allTodo=await dbHelper.fetchAllTodo(filter);
+      notifyListeners();
+    }
+  }
+  fetchInitialTodo ({int filter=0})async{
+    _allTodo=await dbHelper.fetchAllTodo(filter);
+    notifyListeners();
+  }
+
+  isComleted({bool isCompleted=false,
+    required int id , int filter=0})async{
+    bool check =await dbHelper.updateTodoCompleted(id:id,isCompleted:isCompleted);
+
+    if(check){
+      _allTodo=await dbHelper.fetchAllTodo(filter);
+      notifyListeners();
+    }}
+
+}
