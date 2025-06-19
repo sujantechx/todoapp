6496b38bcbfd80738e68db8cf0b5ccafd3a64374
Sujan <sujantechx@gmail.com>
2025-06-19 11:20:55 +0530

    using provider GMS

--- lib/pages/Home/Home.dart
+++ lib/pages/Home/Home.dart
@@ -39,37 +39,36 @@ DBHelper ? dbHelper;
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       body: SafeArea(
-        
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(child: ElevatedButton(onPressed: (){
-                    filter=2;
-                    Provider.of<DbProvider>(context, listen: false).fetchInitialTodo(filter: filter);
-                    // context.read<DbProvider>().featchAllTodo(filter: filter);
-                  }, child: Text('Pending'))),
-                  SizedBox(
-                    width: 11,
-                  ),
-                  Expanded(child: ElevatedButton(onPressed: (){
-                    filter=1;
-                    Provider.of<DbProvider>(context, listen: false).fetchInitialTodo(filter: filter);
-                    // context.read<DbProvider>().featchAllTodo(filter: filter);
-                  }, child: Text('Completed'))),
-                  SizedBox(
-                    width: 11,
-                  ),
-                  Expanded(child: ElevatedButton(onPressed: (){
-                    filter=0;
-                    Provider.of<DbProvider>(context, listen: false).fetchInitialTodo();
-                    // context.read<DbProvider>().featchAllTodo(filter: filter);
-                  }, child: Text('All'))),
-                ],
-              ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: ElevatedButton(onPressed: (){
+                  filter=2;
+                  Provider.of<DbProvider>(context, listen: false).fetchInitialTodo(filter: filter);
+                  // context.read<DbProvider>().featchAllTodo(filter: filter);
+                }, child: Text('Pending'))),
+                SizedBox(
+                  width: 11,
+                ),
+                Expanded(child: ElevatedButton(onPressed: (){
+                  filter=1;
+                  Provider.of<DbProvider>(context, listen: false).fetchInitialTodo(filter: filter);
+                  // context.read<DbProvider>().featchAllTodo(filter: filter);
+                }, child: Text('Completed'))),
+                SizedBox(
+                  width: 11,
+                ),
+                Expanded(child: ElevatedButton(onPressed: (){
+                  filter=0;
+                  Provider.of<DbProvider>(context, listen: false).fetchInitialTodo();
+                  // context.read<DbProvider>().featchAllTodo(filter: filter);
+                }, child: Text('All'))),
+              ],
+            ),
 
-              Consumer<DbProvider>(
+            Expanded(
+              child: Consumer<DbProvider>(
                 builder: (ctx, provider, __){
                   ///mTodo = ctx.watch<DbProvider>().getData();
                   mTodo = provider.getData();
@@ -78,7 +77,7 @@ DBHelper ? dbHelper;
                        itemCount: mTodo.length,
                        itemBuilder: (_, index){
                          Color bgColor = Colors.grey.shade200;
-
+              
                          if(mTodo[index][DBHelper.c_todoPriority]==1){
                            bgColor = Colors.grey.shade200;
                          } else if(mTodo[index][DBHelper.c_todoPriority]==2){
@@ -118,9 +117,9 @@ DBHelper ? dbHelper;
                            }),
                          );
                        })
-                       : Center(child: Text('No Todo'));})
-                   ],
-          ),
+                       : Center(child: Text('No Todo'));}),
+            )
+                 ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/pages/tasks/all_task.dart
+++ lib/pages/tasks/all_task.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/datbase/database_helper.dart';
+import 'package:todoapp/pages/add/add_task.dart';
 
 class AllTask extends StatefulWidget {
   const AllTask({super.key});
@@ -25,72 +26,71 @@ class _AllTaskState extends State<AllTask> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Expanded(
-          child:allTodo.isNotEmpty? ListView.builder(
-            itemCount: allTodo.length,
-            itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Container(
-                height: 150,
-                width: double.infinity,
-                decoration:BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(allTodo[index][DBHelper.c_todoTitle],style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
-                        ),),
-
-                        Text(allTodo[index][DBHelper.c_todoDesc],style: TextStyle(
-                          fontSize: 12,
-                        ),),
-                        SizedBox(height: 10,),
-
-                        Text(allTodo[index][DBHelper.c_todoTaskDeadline],style: TextStyle(
-                          fontSize: 12,
-                        ),),
-
-
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(Icons.edit),
-
-                        SizedBox(height: 10,),
-
-                        Row(
-                          children: [
-                            Text("Mark as complete  ",style: TextStyle(
-                              fontSize: 12,
-                            ),),
-                            Icon(Icons.check_box_outline_blank)
-                          ],
-                        ),
-
-
-                      ],
-                    )
-
-                  ],
-                ),
+        child: allTodo.isNotEmpty? ListView.builder(
+          itemCount: allTodo.length,
+          itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Container(
+              height: 150,
+              width: double.infinity,
+              decoration:BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
               ),
-            );
-        
-          },):Center(
-            child: Text("No ToDo List!!"),
-          ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(allTodo[index][DBHelper.c_todoTitle],style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      ),),
+
+                      Text(allTodo[index][DBHelper.c_todoDesc],style: TextStyle(
+                        fontSize: 12,
+                      ),),
+                      SizedBox(height: 10,),
+
+                      Text(allTodo[index][DBHelper.c_todoTaskDeadline],style: TextStyle(
+                        fontSize: 12,
+                      ),),
+
+
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => AddTask(),));
+                      }, icon: Icon(Icons.edit)),
+                      SizedBox(height: 10,),
+
+                      Row(
+                        children: [
+                          Text("Mark as complete  ",style: TextStyle(
+                            fontSize: 12,
+                          ),),
+                          Icon(Icons.check_box_outline_blank)
+                        ],
+                      ),
+
+
+                    ],
+                  )
+
+                ],
+              ),
+            ),
+          );
+
+        },):Center(
+          child: Text("No ToDo List!!"),
         ),
       ),
     );
