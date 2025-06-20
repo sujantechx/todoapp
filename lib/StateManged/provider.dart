1fc3f8117c87fb2f2fc4e9a6e8ab40f2d3bb09c8
Sujan <sujantechx@gmail.com>
2025-06-20 16:51:11 +0530

    todo complete provider

--- lib/StateManged/provider.dart
+++ lib/StateManged/provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/datbase/database_helper.dart';
+import '../DataBase/database_sqlite.dart';
 
 class DbProvider extends ChangeNotifier{
   DbProvider({required this.dbHelper});
